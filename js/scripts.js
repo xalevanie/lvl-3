919e55b18c54adf86b4113b0c2dacd044f1ef280
Madeline <Dawndylion@Dawns-iMac.local>
2017-05-14 21:53:07 -0700

    Learned
    document.documentElement.style.setProperty(‘—${this.name}’, this.value
    + suffix);
    this.name doesn’t work, why? Using the actual names of the variables
    instead, so at least it works now.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,11 @@ console.log('Whaaat');
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
+    console.log(this.name);
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty('--${this.name}', this.value + suffix);
+    document.documentElement.style.setProperty('--spacing', this.value + suffix);
+    document.documentElement.style.setProperty('--blur', this.value + suffix);
+    document.documentElement.style.setProperty('--base', this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
