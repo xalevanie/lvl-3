63e260e1c64844a7ede9e4e10e848badd4e9583e
Madeline <Dawndylion@Dawns-iMac.local>
2017-05-14 20:58:42 -0700

    using updated web3starter

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// @codekit-prepend "jquery.js";
+// @codekit-prepend "semantic.js";
+
+console.log('Whaaat');
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty('--${this.name}', this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
